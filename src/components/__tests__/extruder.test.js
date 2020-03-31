c978ca5511de07aff0508f3f598fdab9cf713403
Veselina Radeva <veselina.radeva@telerik.com>
2020-03-31 23:06:07 +0300

    test: extruder tests

---
+++ src/components/__tests__/extruder.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Extruder from '../extruder';
+import { render, cleanup } from '@testing-library/react';
+
+afterEach(cleanup);
+
+it("renders extruder correctly", () => {
+    const {queryByTestId, container} = render(
+        <Extruder 
+            shouldPushNewBiscuit = {false}
+            isMachineMovementPaused = {false}
+        />
+    );
+    
+    expect(container).toMatchSnapshot();
+    expect(queryByTestId("biscuit-dough-svg")).not.toBeInTheDocument();
+});
+
+it("renders biscuit correctly", () => {
+    const {queryByTestId} = render(
+        <Extruder 
+            shouldPushNewBiscuit = {true}
+            isMachineMovementPaused = {false}
+        />
+    );
+    
+    expect(queryByTestId("biscuit-dough-svg")).toBeInTheDocument();
+});
+
+it("renders biscuit correctly when machine is paused", () => {
+    const {queryByTestId} = render(
+        <Extruder 
+            shouldPushNewBiscuit = {true}
+            isMachineMovementPaused = {true}
+        />
+    );
+    
+    expect(queryByTestId("biscuit-dough-svg")).toBeInTheDocument();
+    expect(queryByTestId("biscuit-dough-svg")).toHaveClass("animation-paused");
+});
+
+
