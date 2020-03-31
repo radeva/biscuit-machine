560d51bf8d968045e80af55f806981b32dad4f5f
Veselina Radeva <veselina.radeva@telerik.com>
2020-03-31 23:16:05 +0300

    test: add conveyor tests

---
+++ src/components/__tests__/conveyor.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Conveyor from '../conveyor';
+import { render, cleanup } from '@testing-library/react';
+
+afterEach(cleanup);
+
+it("renders conveyor correctly when off", () => {
+    const {container} = render(
+        <Conveyor isOn = {false} />
+    );
+    
+    expect(container).toMatchSnapshot();
+});
+
+it("renders conveyor correctly when on", () => {
+    const {container} = render(
+        <Conveyor isOn = {true} />
+    );
+    
+    expect(container).toMatchSnapshot();
+});
