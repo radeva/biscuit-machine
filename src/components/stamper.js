5f2089707ca1bac343c9598165350ada5948d702
Veselina Radeva <veselina.radeva@telerik.com>
2020-03-31 22:57:19 +0300

    refactor: add prop types

--- src/components/stamper.js
+++ src/components/stamper.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {ReactComponent as StamperSVG} from './../images/stamper.svg';
 import {ReactComponent as StamperPusherSVG} from './../images/stamper-pusher.svg';
 import {ReactComponent as RawBiscuitSVG} from './../images/raw-biscuit.svg';
@@ -19,14 +20,27 @@ export default function Stamper(props) {
 
     if(props.hasBiscuitToStamp) {
         doughElement=(
-            <RawBiscuitSVG className={'raw-biscuit ' + animationPausedClass}/>
+            <RawBiscuitSVG data-testid='raw-biscuit' className={'raw-biscuit ' + animationPausedClass}/>
         );
     }
     return (
         <div className='stamper left'>
                 <StamperSVG />
-                <StamperPusherSVG className={stamperPusherClassName} />
+                <StamperPusherSVG data-testid='stamper-pusher' className={stamperPusherClassName} />
             {doughElement}
         </div>
     );
+}
+
+
+Stamper.propTypes = {
+    isMachineMovementOn: PropTypes.bool,
+    isMachineMovementPaused: PropTypes.bool,
+    hasBiscuitToStamp: PropTypes.bool
+}
+
+Stamper.defaultProps = {
+    isMachineMovementOn: false,
+    isMachineMovementPaused: false,
+    hasBiscuitToStamp: false
 }
