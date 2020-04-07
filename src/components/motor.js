3b9d74b0c3ae9c3bc99b063eaa26bdac147f4d7a
Veselina Radeva <veselina.radeva@telerik.com>
2020-04-07 23:31:43 +0300

    fix: timeouts when pausing the machine

--- src/components/motor.js
+++ src/components/motor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ReactComponent as MotorSVG } from './../images/motor.svg';
 import useInterval from 'react-useinterval';
@@ -6,31 +6,46 @@ import useInterval from 'react-useinterval';
 export const MOTOR_TIMEOUT = 4000;
 
 export default function Motor(props) {
+  const [intervalOffset, setIntervalOffset] = useState(0),
+    [lastIntervalTime, setLastIntervalTime] = useState(new Date());
+
   let containerCss = 'motor';
+  containerCss += props.isOn ? ' motor-on' : '';
+  containerCss += props.isMachineMovementPaused
+    ? ' motor-on animation-paused'
+    : '';
 
-  if (props.isMachineMovementPaused) {
-    containerCss += ' motor-on animation-paused';
-  }
-  if (props.isOn) {
-    containerCss += ' motor-on';
-  }
-  
   const handlePulse = () => {
-    if (props.isMachineMovementPaused) {
-      return;
-    }
-
+    setLastIntervalTime(new Date());
     props.onSendPulse();
   };
 
   useInterval(
     handlePulse,
-    props.isOn || props.isMachineMovementPaused ? MOTOR_TIMEOUT : null,
+    (props.isOn || props.isMachineMovementPaused) && intervalOffset === 0
+      ? MOTOR_TIMEOUT
+      : null,
   );
+
+  useEffect(() => {
+    if (props.isMachineMovementPaused) {
+      if (intervalOffset === 0) {
+        const offset =
+          MOTOR_TIMEOUT - (new Date().getTime() - lastIntervalTime.getTime());
+        setIntervalOffset(Math.abs(offset));
+      }
+    } else if (intervalOffset > 0) {
+      setTimeout(() => {
+        setIntervalOffset(0);
+        setLastIntervalTime(new Date());
+        props.onSendPulse();
+      }, intervalOffset);
+    }
+  }, [props, intervalOffset, lastIntervalTime]);
+
   return (
     <div data-testid="motor-container" className={containerCss}>
-      {' '}
-      <MotorSVG className="motor-svg" />{' '}
+      <MotorSVG className="motor-svg" />
     </div>
   );
 }
